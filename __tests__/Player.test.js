7b8c3b2f0aec6dfc78f1e2a3f78a8c04c613539c
Tanya Jurecki <tljurecki@gmail.com>
2022-04-18 16:13:40 -0400

    created Player object

---
+++ __tests__/Player.test.js
@@ -0,0 +1,17 @@
+const Potion = require('../lin/Potion');
+
+jest.mock('../lib/Potion');
+const { test, expect, jest } = require('@jest/globals');
+const Player = require('../lib/Player');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
